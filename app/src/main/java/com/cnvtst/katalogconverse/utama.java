c4747ae1ded0e308ed0d1a008089650164e03607
reyhanrere22 <reyhanregan22@gmail.com>
2020-02-05 17:14:23 +0700

    Initial commit

---
+++ app/src/main/java/com/cnvtst/katalogconverse/utama.java
@@ -0,0 +1,13 @@
+package com.cnvtst.katalogconverse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class utama extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utama);
+    }
+}
